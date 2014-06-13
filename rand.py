f963c06b1758f94ef1d48573bc18ea1e8ba3e58c
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2014-06-13 21:44:08 +0300

    Find loops in sequence

---
+++ rand.py
@@ -0,0 +1,40 @@
+# pseudo random number generators
+__author__ = 'Ruslanas'
+
+def middle_square(seed, digits=4):
+    square = seed * seed
+    half_digits = digits >> 1
+    return int(square % 10**(digits + half_digits) / 10**half_digits)
+
+def print_sequence(seed, length=10):
+    n = seed
+    for i in range(length):
+        n = middle_square(n)
+        print(n)
+
+
+def find_loops(length=5, digits=4):
+    """
+    Find circular sequences less than length long
+    :param length:
+    :param digits:
+    :return:
+    """
+    bad_seeds = []
+    for seed in range(10**digits - 1):
+        n = seed
+        for k in range(length):
+            n = middle_square(n)
+            # loop closed or known loop encountered
+            bad = n in bad_seeds
+            if n == seed or bad:
+                if not bad:
+                    bad_seeds.append(seed)
+                    print("%d in %d steps closes" % (seed, k+1))
+                else:
+                    print("%d in %d steps reaches bad seed %d" % (seed, k+1, n))
+                break
+    return bad_seeds
+
+if __name__ == '__main__':
+    print(find_loops())
