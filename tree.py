2321456fa48a13e3b7ad423cfe65bda31b77e86e
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2014-06-17 04:29:24 +0300

    Little forest with to families of same species.

--- tree.py
+++ tree.py
@@ -4,10 +4,12 @@ from tkinter import *
 import random
 import math
 
+
 class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def rotate(self, origin, angle):
         dx = self.x - origin.x
         dy = self.y - origin.y
@@ -18,41 +20,62 @@ class Point():
         self.x += origin.x
         self.y += origin.y
 
-def branch(start, levels=7, length=100, width=1, start_angle=0):
+
+def branch(start, levels=7, length=100, width=1, start_angle=0, trunk=False):
     if levels > 0:
 
-        var = 0.8
         end = Point(start.x, start.y - random.randint(int(var * length), length))
-
-        angle = start_angle + random.randint(-30, 30)
+        if trunk:
+            angle = 0
+        else:
+            angle = start_angle + random.randint(-70, 70)
         end.rotate(start, angle)
 
-        canvas.create_line(start.x, start.y, end.x, end.y, width=width, tags=3)
-        length *= 0.618 # golder ratio
-        width *= 0.618
+        width /= 1 + 1/levels
 
-        for i in range(random.randint(2,3)):
+        canvas.create_line(start.x, start.y, end.x, end.y, width=width, tags=3)
+        length *= 0.618  # golder ratio
+
+        for i in range(random.randint(2, 3)):
             branch(end, random.randint(levels - 2, levels - 1), int(length), width, angle)
+
+def seed(start, generations, height, distance):
+    dead_zone = 10
+
+    for i in range(1, generations):
+        branch(start, trunk=True, width=3, levels=generations - i,
+               length=random.randint(int(var * height), int(height)))
+
+        # drop seed both directions
+
+        start.x += random.randint(int(-distance), int(distance))
+
+        # younger trees
+        height *= golden_ratio
+        distance *= golden_ratio
+
+        if round(dead_zone) <= 0 or round(distance) <= 0:
+            break
+
 
 if __name__ == '__main__':
     root = Tk()
+    root.title('Forest')
 
-    w = 600
-    h = 300
+    w = 800
+    h = 500
 
     canvas = Canvas(root, width=w, height=h)
     canvas.pack()
 
-    start = Point(70, h)
+    # parent
 
-    branch_length = 50
-    height = 160 # parent height
-    var = 0.7
-    for i in range(7):
-        branch(start, width=3, length=random.randint(int(var * height), int(height)))
-        # drop seed
-        start.x += random.randint(20, 130)
-        # seed generation
-        height = 0.8 * height
+    var = 0.7  # variability
+
+    golden_ratio = 0.8
+
+    # plant
+    seed(Point(w/2, h), generations=10, height=160, distance=240)
+    seed(Point(w/2 + 150, h), generations=10, height=100, distance=100)
 
     mainloop()
