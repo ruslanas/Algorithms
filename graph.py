27940623f96995238979d911c9a0b95743e9803b
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2014-07-01 00:41:50 +0300

    Demo

--- graph.py
+++ graph.py
@@ -6,10 +6,13 @@ from tkinter import *
 root = Tk()
 
 canvas = Canvas(root, width=100, height=100)
+canvas.pack()
 
 vec = Vec3(0, 50, 0)
-canvas.pack()
-for i in range(9):
-    vec.rotate_x(5)
-    canvas.create_line(0, 0, vec.y, vec.z)
+origin = Vec3(50, 50, 0)
+
+for i in range(12):
+    canvas.create_line(origin.x, origin.y, (origin + vec).x, (origin + vec).y)
+    vec.rotate_z(30)
+
 mainloop()
--- graphics/vector.py
+++ graphics/vector.py
@@ -1,7 +1,7 @@
 __author__ = 'Ruslanas'
 
 from graphics.matrix import Matrix
-from math import sin, cos, pi
+from math import sin, cos, pi, sqrt
 
 
 class Vec3():
@@ -9,6 +9,30 @@ class Vec3():
         self.x = x
         self.y = y
         self.z = z
+
+    def __abs__(self):
+        return self.length()
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __repr__(self):
+        return "graphics.vector.Vec3(%f, %f, %f)" % (self.x, self.y, self.z)
+
+    def rotate_z(self, angle):
+        angle = angle * pi / 180
+        vector_matrix = Matrix([[self.x, self.y, self.z]])
+        vector_matrix.transpose()
+        rotation_matrix = Matrix([
+            [cos(angle), -sin(angle), 0],
+            [sin(angle), cos(angle), 0],
+            [0, 0, 1]
+        ])
+        vec = rotation_matrix * vector_matrix
+        self.x = vec(1,1)
+        self.y = vec(2,1)
+        self.z = vec(3,1)
+        return self
 
     def rotate_x(self, angle):
         angle = angle * pi / 180
@@ -25,7 +49,19 @@ class Vec3():
         self.z = vec(3,1)
         return self
 
+    def length(self):
+        return sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+
+
+def dot(a, b):
+    return a.x * b.x + a.y * b.y + a.z * b.z
+
 
 if __name__ == '__main__':
-    v = Vec3(0, 1, 0)
-    print(v.rotate_x(45))
+    v1 = Vec3(0.0, 1.0, 0.0)
+    v2 = Vec3(1.0, 0.0, 0.0)
+    print(dot(v1, v2))
+    print(v1.rotate_x(45))
+    print(Vec3(1,1,1).length())
+    print(v1 + v2)
+    print(abs(v2))
